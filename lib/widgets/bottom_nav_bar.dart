3a1d5910ff9692bc1653f28cee84658de135b93f
smridhi katyal <smridhi.katyal245@gmail.com>
2024-03-30 23:43:08 +0530

    First Commit

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meditation_app/constants.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+      height: 80,
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          BottomNavItem(
+            press: (){},
+            title: "Today",
+            svgScr: "assets/icons/calendar.svg",
+          ),
+          BottomNavItem(
+            press: (){},
+            title: "All Exercises",
+            svgScr: "assets/icons/gym.svg",
+            isActive: true,
+          ),
+          BottomNavItem(
+            press: (){},
+            title: "Settings",
+            svgScr: "assets/icons/Settings.svg",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomNavItem extends StatelessWidget {
+  final String svgScr;
+  final String title;
+  final Function press;
+  final bool isActive;
+  const BottomNavItem({
+    Key? key,
+    required this.svgScr,
+    required this.title,
+    required this.press,
+    this.isActive = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){},
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          SvgPicture.asset(
+            svgScr,
+          colorFilter: ColorFilter.mode(
+          isActive ? kActiveIconColor : kTextColor,
+          BlendMode.srcIn,
+          ),
+          ),
+          Text(
+            title,
+            style: TextStyle(color: isActive ? kActiveIconColor : kTextColor),
+          ),
+        ],
+      ),
+    );
+  }
+}
